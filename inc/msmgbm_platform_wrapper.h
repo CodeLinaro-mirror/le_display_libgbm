fe1a8087af875a93eb07fc93804d8021736fb95d
Petar G. Georgiev <quic_petarg@quicinc.com>
2022-11-18 14:56:53 +0200

    libgbm: Add perform tag for getting buffer stride, scanline and size
    Add tag called GBM_PERFORM_GET_BUFFER_STRIDE_SCANLINE_SIZE for querying
    buffer stride, scanline and size.
    
    Change-Id: Ia2282c0739aec03f8edc529ef3732ad5e158d45b

--- inc/msmgbm_platform_wrapper.h
+++ inc/msmgbm_platform_wrapper.h
@@ -1,4 +1,7 @@
 /*
+* Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+* Not a Contribution.
+*
 * Copyright (c) 2017, 2021 The Linux Foundation. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
@@ -120,6 +123,15 @@ class platform_wrap {
    */
   int is_valid_rgb_fmt(int gbm_format);
 
+  /**
+   * Function to check whether the format is RAW
+   * @params    gbm format
+   * @return    true : success
+   *            false : fail
+   *
+   */
+  bool is_valid_raw_fmt(int format);
+
   uint32_t get_bpp_for_uncmprsd_rgb_fmt(int format);
 
   /**
@@ -150,6 +162,15 @@ class platform_wrap {
    */
   void get_aligned_wdth_hght(gbm_bufdesc *descriptor, unsigned int *aligned_w,
                                       unsigned int *aligned_h);
+
+  /**
+   * Function to get stride, scanline and size depending on the underlying GPU/Video platform
+   * @return    stride
+   *            scanline
+   *            size
+   */
+  void get_stride_scanline_size(gbm_bufdesc *descriptor, unsigned int *stride,
+                                unsigned int *scanline, unsigned int *size);
 
   /**
    * Function to get  size aligned width and height depending on the underlying GPU/Video platform
@@ -190,6 +211,15 @@ extern "C" {
     *            aligned_h
     */
     void qry_aligned_wdth_hght(gbm_bufdesc *descriptor, unsigned int *alignedw, unsigned int *alignedh);
+
+    /**
+    * C wrapper Function to get stride, scanline and size depending on the underlying GPU/Video platform
+    * @return    stride
+    *            scanline
+    *            size
+    */
+    void qry_stride_scanline_size(gbm_bufdesc *descriptor, unsigned int *stride,
+                                  unsigned int *scanline, unsigned int *size);
 
     /**
      * C wrapper Function to query size based on format from the platform wrapper
@@ -233,9 +263,17 @@ extern "C" {
     /**
      * C wrapper function to know if the format is UBWC
      */
-
     bool is_ubwc_enbld(int format, int prod_usage,
                               int cons_usage);
+
+    /**
+    * C wrapper Function to check whether the format is RAW format or not.
+    * @params    gbm format
+    * @return    boolean 0 (non RAW format)
+    *                    1 (RAW format)
+    */
+    bool is_valid_raw_format(int format);
+
     /**
      * C wrapper function to know if the format is RGB
      */
