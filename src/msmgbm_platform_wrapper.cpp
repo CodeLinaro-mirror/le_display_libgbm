dfcf661039fae15b5972f5df044e2b9800a457ca
Avdhesh Signh Chauhan <quic_avdheshs@quicinc.com>
2023-01-06 15:57:57 +0530

    gbm: Do not align when GBM_BO_USAGE_NO_ALLIGNMENT flag is passed.
    Change-Id: I07335482de2bb09f4bd7dd8c161d115d6549c328

--- src/msmgbm_platform_wrapper.cpp
+++ src/msmgbm_platform_wrapper.cpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+* Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
 * Not a Contribution.
 *
 * Copyright (c) 2018, 2021 The Linux Foundation. All rights reserved.
@@ -623,6 +623,7 @@ void platform_wrap::get_aligned_wdth_hght(gbm_bufdesc *descriptor, unsigned int 
   if (descriptor->Usage & GBM_BO_USAGE_NO_ALLIGNMENT) {
     *alignedw = width;
     *alignedh = height;
+    return;
   }
 
   if (CameraInfo::GetInstance()->IsCameraCustomFormat(format)) {
