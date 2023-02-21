9003d348c07f527753d790570c749fd541fe7e95
fangez <quic_fangez@quicinc.com>
2023-02-21 17:19:52 +0800

    libgbm: refine gem handle flow
    add gem handle map to trace the reference
    The ioctl PRIME_FD_HANDLE may return the same handle by the same devicefd and
    dmabuf. If we destroy handle in each gbm_bo_destroy() will cause some handle
    in use to be closed.
    To fix this, save the reference count of gem handle in the map and only close the
    handle when the reference count is 0.
    
    Change-Id: Ib8b9ff14f9d6e26e84b7c34b1726a8c21d19c3b6

--- inc/msmgbm.h
+++ inc/msmgbm.h
@@ -506,6 +506,24 @@ void config_dbg_lvl(void);
 void msmgbm_log_hdr_color_info_mdata(ColorMetaData * color_mdata);
 void msmsgbm_default_init_hdr_color_info_mdata(ColorMetaData * color_mdata);
 
+/**
+  * C wrapper function to increment the reference count for the valid gem_handle
+  * @input param: device fd
+  * @input param: gem handle
+  * @return     : none
+  *
+  */
+void incr_handle_refcnt(int device_fd, uint32_t handle);
+
+/**
+  * C wrapper function to decrement the reference count for the valid map gem_handle
+  * @input param: device fd
+  * @input param: gem handle
+  * @return     : 1 for delete key from map /0 for decremented ref count
+  *
+  */
+int decr_handle_refcnt(int device_fd, uint32_t handle);
+
 #ifdef __cplusplus
 }
 #endif
