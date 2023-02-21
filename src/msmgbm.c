71d8dc58e94076b2668bf8be1be1522f026ea1db
fangez <quic_fangez@quicinc.com>
2023-02-21 14:48:43 +0800

    libgbm: Tune the logic of gbm_bo_get_fd()
    1.Duplicate fd in msmgbm_bo_get_fd() to be aligned with mesa gbm.
    
    each call to gbm_bo_get_fd() returns a new
    file descriptor and the caller is responsible for closing the fd
    
    2.add gbmtest
    Test result as below
    run gbmtest_all.sh
    usage: gbmtest_all <Max Test cases to execute>
    Default Max Tests are 27
    killall: weston: no process killed
    TEST 1 - PASSED
    TEST 2 - PASSED
    TEST 3 - PASSED
    TEST 4 - PASSED
    TEST 5 - PASSED
    TEST 6 - PASSED
    TEST 7 - FAILED
    TEST 8 - PASSED
    TEST 9 - PASSED
    TEST 10 - PASSED
    TEST 11 - PASSED
    TEST 12 - FAILED
    TEST 13 - PASSED
    TEST 14 - PASSED
    TEST 15 - PASSED
    TEST 16 - PASSED
    TEST 17 - PASSED
    TEST 18 - PASSED
    TEST 19 - PASSED
    TEST 20 - PASSED
    TEST 21 - PASSED
    TEST 22 - PASSED
    TEST 23 - PASSED
    TEST 24 - PASSED
    TEST 25 - PASSED
    TEST 26 - PASSED
    TEST 27 - PASSED
    
    RESULTS:
    Passing Test Cases:
    1 2 3 4 5 6 8 9 10 11 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27
    
    Failing Test Cases:
     7 12
    
    Change-Id: I3a39b3b789aa76e9e84dc31175923301ddf841ed

--- src/msmgbm.c
+++ src/msmgbm.c
@@ -1,7 +1,4 @@
 /*
-* Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
-* Not a Contribution.
-*
 * Copyright (c) 2017 - 2021 The Linux Foundation. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
@@ -242,13 +239,16 @@ msmgbm_bo_unmap(void *map_data)
 static int
 msmgbm_bo_get_fd(struct gbm_bo *bo)
 {
-
-    if(bo!=NULL){
-        return bo->ion_fd;
-    }
-    else {
+    if (bo != NULL) {
+        int new_fd = dup(bo->ion_fd);
+        if (new_fd < 0) {
+            LOG(LOG_ERR, "Fail to dup ion_fd. Err:\n%s\n", strerror(errno));
+            return -1;
+        }
+        return new_fd;
+    } else {
         LOG(LOG_ERR, "NULL or Invalid bo pointer\n");
-    return 0;
+        return -1;
     }
 }
 
