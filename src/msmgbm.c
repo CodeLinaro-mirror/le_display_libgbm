b63eb8faa489cc2931fa4d306990c113257e03a9
Chirag Khurana <quic_ckhurana@quicinc.com>
2023-03-01 18:47:49 +0530

    gbm: populate plane_count member for buffer objects.
    Change-Id: I43008c2cf71ca8a4c4d8c0fa674e3eccbddee350
    (cherry picked from commit a152f90b8722c5c2f763086baf6e9e7cb00564b4)

--- src/msmgbm.c
+++ src/msmgbm.c
@@ -1001,11 +1001,14 @@ msmgbm_bo_import_fd(struct msmgbm_device *msm_dev,
         return NULL;
     }
 
+    int plane_count = msmgbm_get_format_modifier_plane_count(buffer_info->format, 0);
+
     gbmbo                = &msm_gbmbo->base;
     gbmbo->ion_fd        = buffer_info->fd;
     gbmbo->ion_metadata_fd = gbo_info.metadata_fd;
     gbmbo->handle.u32    = gemimport_req.handle;
     gbmbo->usage_flags   = bufdesc.Usage;
+    gbmbo->plane_count   = plane_count;
     gbmbo->format        = buffer_info->format;
     gbmbo->width         = buffer_info->width;
     gbmbo->height        = buffer_info->height;
@@ -1175,11 +1178,14 @@ msmgbm_bo_import_fd_modifier(struct msmgbm_device *msm_dev,
         return NULL;
     }
 
+    int plane_count = msmgbm_get_format_modifier_plane_count(buffer_info->format, 0);
+
     gbmbo                  = &msm_gbmbo->base;
     gbmbo->ion_fd          = buffer_info->fd;
     gbmbo->ion_metadata_fd = buffer_info->metadata_fd;
     gbmbo->handle.u32      = gemimport_req.handle;
     gbmbo->usage_flags     = bufdesc.Usage;
+    gbmbo->plane_count     = plane_count;
     gbmbo->format          = buffer_info->format;
     gbmbo->width           = buffer_info->width;
     gbmbo->height          = buffer_info->height;
@@ -1379,11 +1385,14 @@ msmgbm_bo_import_gbm_buf(struct msmgbm_device *msm_dev,
         return NULL;
     }
 
+    int plane_count = msmgbm_get_format_modifier_plane_count(buffer_info->format, 0);
+
     gbmbo                  = &msm_gbmbo->base;
     gbmbo->ion_fd          = buffer_info->fd;
     gbmbo->ion_metadata_fd = buffer_info->metadata_fd;
     gbmbo->handle.u32      = gemimport_req.handle;
     gbmbo->usage_flags     = bufdesc.Usage;
+    gbmbo->plane_count     = plane_count;
     gbmbo->format          = buffer_info->format;
     gbmbo->width           = buffer_info->width;
     gbmbo->height          = buffer_info->height;
