56b96b3c108d9cd78448f7d318a29f12ddb13b99
Hongyong Huang <quic_hongyong@quicinc.com>
2023-03-08 11:26:53 +0800

    libgbm: Add BGRA8888 format in GBM.
    - Added GBM_FORMAT_BGRA8888 format in libgbm.
    
    Change-Id: I126a898863e87e40d98d10047ca01ab41fc8f419

--- src/msmgbm_platform_wrapper.cpp
+++ src/msmgbm_platform_wrapper.cpp
@@ -341,6 +341,7 @@ int platform_wrap::is_valid_rgb_fmt(int format){
         case GBM_FORMAT_RGB565:
         case GBM_FORMAT_RGB888:
         case GBM_FORMAT_BGR888:
+        case GBM_FORMAT_BGRA8888:
         case GBM_FORMAT_RGBA8888:
         case GBM_FORMAT_RGBX8888:
         case GBM_FORMAT_XRGB8888:
