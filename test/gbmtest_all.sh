838a10b533db9256187410b5546c2f0c633c8666
fangez <quic_fangez@quicinc.com>
2023-05-04 15:06:43 +0800

    gbm: test: add unit test for meta fd retrieval
    usage: gbmtest_all <Max Test cases to execute>
    Default Max Tests are 26
    killall: weston: no process killed
    TEST 1 - PASSED
    TEST 2 - PASSED
    TEST 3 - PASSED
    TEST 4 - PASSED
    TEST 5 - PASSED
    TEST 6 - PASSED
    TEST 7 - PASSED
    TEST 8 - PASSED
    TEST 9 - PASSED
    TEST 10 - PASSED
    TEST 11 - PASSED
    TEST 12 - PASSED
    TEST 13 - PASSED
    TEST 14 - PASSED
    TEST 15 - PASSED
    TEST 16 - PASSED
    TEST 17 - PASSED
    TEST 18 - PASSED
    TEST 19 - PASSED
    TEST 20 - PASSED
    TEST 21 - PASSED
    TEST 22 - PASSED
    TEST 23 - PASSED
    TEST 24 - PASSED
    TEST 25 - PASSED
    TEST 26 - PASSED
    
    RESULTS:
    Passing Test Cases:
    
    1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26
    
    Failing Test Cases:
    
    Change-Id: I3068512eecd7a30e3453601600d71018abff8550

--- test/gbmtest_all.sh
+++ test/gbmtest_all.sh
@@ -38,8 +38,8 @@ res=1
 if [ ! "$1" ]
 then
 	echo "usage: gbmtest_all <Max Test cases to execute>"
-	echo "Default Max Tests are 25"
-	MAX_TEST_CASE=25
+	echo "Default Max Tests are 26"
+	MAX_TEST_CASE=26
 else
 	MAX_TEST_CASE=$1
 fi
