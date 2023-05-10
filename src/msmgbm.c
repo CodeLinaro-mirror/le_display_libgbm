7a5aeeeb49612b659d89bd0ccf24590a210cf760
Chirag Khurana <quic_ckhurana@quicinc.com>
2023-05-10 20:39:22 +0530

    libgbm: cycle buffers for gbm_surface
    Change-Id: Ibc78c2f5d1eb2f7d647faf8df032d143a821b591

--- src/msmgbm.c
+++ src/msmgbm.c
@@ -1755,11 +1755,12 @@ msmgbm_surface_lock_front_buffer(struct gbm_surface *surf)
 #ifdef ALLOCATE_SURFACE_BO_AT_CREATION
         for(index =0; index < NUM_BACK_BUFFERS; index++)
         {
-            if((msm_gbm_surface->bo[index]!= NULL) && \
-                (msm_gbm_surface->bo[index]->current_state == GBM_BO_STATE_NEW_FRONT_BUFFER))
+            int cur_index = (msm_gbm_surface->inuse_index + index + 1) % NUM_BACK_BUFFERS;
+            if((msm_gbm_surface->bo[cur_index]!= NULL) && \
+                (msm_gbm_surface->bo[cur_index]->current_state == GBM_BO_STATE_NEW_FRONT_BUFFER))
             {
-                msm_gbm_surface->bo[index]->current_state = GBM_BO_STATE_INUSE_BY_COMPOSITOR;
-                return &msm_gbm_surface->bo[index]->base;
+                msm_gbm_surface->bo[cur_index]->current_state = GBM_BO_STATE_INUSE_BY_COMPOSITOR;
+                return &msm_gbm_surface->bo[cur_index]->base;
             }
         }
         LOG(LOG_ERR,"No Front BO found\n");
@@ -2077,18 +2078,18 @@ struct gbm_bo* msmgbm_surface_get_free_bo(struct gbm_surface *surf)
 
     if(msm_gbm_surface != NULL)
     {
-            int cur_index = msm_gbm_surface->inuse_index;
-            for(index = ((cur_index + 1) % NUM_BACK_BUFFERS); index < NUM_BACK_BUFFERS; index++)
+        for(index = 0; index < NUM_BACK_BUFFERS; index++)
+        {
+            int cur_index = (msm_gbm_surface->inuse_index + index + 1) % NUM_BACK_BUFFERS;
+            if((msm_gbm_surface->bo[cur_index]!= NULL) && \
+                (msm_gbm_surface->bo[cur_index]->current_state == GBM_BO_STATE_FREE))
             {
-                if((msm_gbm_surface->bo[index]!= NULL) && \
-                    (msm_gbm_surface->bo[index]->current_state == GBM_BO_STATE_FREE))
-                {
-                    msm_gbm_surface->bo[index]->current_state = GBM_BO_STATE_INUSE_BY_GPU;
-                    msm_gbm_surface->inuse_index = index;
-                    return &msm_gbm_surface->bo[index]->base;
-                }
+                msm_gbm_surface->bo[cur_index]->current_state = GBM_BO_STATE_INUSE_BY_GPU;
+                msm_gbm_surface->inuse_index = cur_index;
+                return &msm_gbm_surface->bo[cur_index]->base;
             }
-            LOG(LOG_ERR," NO Free BO found!!\n");
+        }
+        LOG(LOG_ERR," NO Free BO found!!\n");
     }
     else
     {
