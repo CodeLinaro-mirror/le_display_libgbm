3cff4f15b3bbe21b1cdd3c83c3ff596de82d932d
Rajavenu Kyatham <quic_rkyatham@quicinc.com>
2024-03-07 15:25:06 +0530

    gbm: add support to FLEX formats.
    Change-Id: Ibe6b212041c0c13bd41d8cf97f341cc818b5e7d9

--- inc/msmgbm_platform_wrapper.h
+++ inc/msmgbm_platform_wrapper.h
@@ -29,7 +29,7 @@
 
 /*
 * Changes from Qualcomm Innovation Center are provided under the following license:
-* Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+* Copyright (c) 2023-2024 Qualcomm Innovation Center, Inc. All rights reserved.
 * SPDX-License-Identifier: BSD-3-Clause-Clear
 */
 
@@ -213,6 +213,21 @@ class platform_wrap {
    */
     void get_yuv_ubwc_wdth_hght(int width, int height, int format,
                                           unsigned int *aligned_w, unsigned int *aligned_h);
+
+  /**
+   * Function to query UBWC FLEX format or not
+   * @return   true : success
+   *           false : fail
+   */
+    bool is_ubwc_flex_format(int format);
+
+  /**
+   * Function to get batch size for FLEX format
+   * @params    gbm format
+   * @return    batch size
+   *
+   */
+    uint32_t get_batch_size(int format);
 
  private:
   bool gpu_support_macrotile = false;
